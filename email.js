8d6b9938040d9699bb544f2c96a20b1561961651
alif <alyfahmad@gmail.com>
2022-11-11 21:27:22 +0600

    fixed contact form issue

--- email.js
+++ email.js
@@ -9,16 +9,19 @@ function sendMail() {
     const serviceID = "service_u5kfwip";
     const templateID = "template_3goia77";
 
-    emailjs.send(serviceID,templateID,params)
-    .then(
-        res =>{
-            document.getElementById("from_name").value="";
-            document.getElementById("email").value="";
-            document.getElementById("phone").value="";
-            document.getElementById("message").value="";
-            console.log("something happened")
-            alert("Message sent successfully");
-        }
-    )
-    .catch((err) => console.log(err));
+
+    if(params.from_name !== '' && params.phone !== '') {
+        emailjs.send(serviceID, templateID, params)
+        // .then(
+        //     res =>{
+        //         // document.getElementById("from_name").value="";
+        //         // document.getElementById("email").value="";
+        //         // document.getElementById("phone").value="";
+        //         // document.getElementById("message").value="";
+        //         console.log(serviceID,templateID,params);
+        //         alert("Message sent successfully");
+        //     }
+        // )
+        // .catch((err) => console.log(err));
+    }
 }
